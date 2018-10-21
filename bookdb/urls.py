3809883d7caf8d5b1a8b7f980cbaf0ecaf990e4c
Parth Doshi <studentparth18@gmail.com>
2018-10-21 17:16:37 +0530

    added book detail view

--- bookdb/urls.py
+++ bookdb/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 app_name = 'bookdb'
 urlpatterns = [
 	path('home/', views.login_or_register, name='home'),
+	path('book/<str:isbn>/', views.book_detail, name='book_detail'),
 ]
--- bookdb/views.py
+++ bookdb/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from django.db.models import Max
+from django.db.models import Max, Avg
 from django.contrib.auth import login, authenticate
 
 def home(request):
@@ -43,3 +43,15 @@ def login_or_register(request):
 		else:
 			# TODO: return some specific recommendations here
 			return render(request, 'index.html', {})
+
+def book_detail(request, isbn):
+	if not request.user.is_authenticated:
+		return redirect('bookdb:home')
+	try:
+		book = Book.objects.get(isbn=isbn)
+		comments = Comment.objects.filter(book=book).order_by('-time_of_comment')
+		avg_rating = Rating.objects.filter(book=book).aggregate(Avg('rating'))['rating__avg']
+		return render(request, 'book_detail.html', {'isbn': isbn, 'comments': comments, 'avg_rating': avg_rating})
+	except Exception as e:
+		print(e)
+	return redirect('bookdb:home')
